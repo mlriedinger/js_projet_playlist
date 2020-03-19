a5bea091a87ba99c1db77757276347d01b56a143
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-03-19 14:53:09 +0100

    Exercice 1 complété

--- 1-decouvrir-DOM-jquery/niveau1/script.js
+++ 1-decouvrir-DOM-jquery/niveau1/script.js
@@ -1,1 +1,17 @@
+var datasplitted;
+
+filmList = $.get('./playlist.txt', function(data){
+    datasplitted = (data.split('\n')) ;
+    console.log(datasplitted);
+});
+
+$(document).ready(function(){
+    $("#ui").children("button").click(function(){
+        for (i=0; i<datasplitted.length; i++){
+            $('#list').children('ul').append('<li>' + datasplitted [i]+ '</li>');
+        };
+    });
+});
+
+
 
