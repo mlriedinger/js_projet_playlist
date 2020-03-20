ad2b5082f47fcc24563aff5ebc498341a3ab5d4c
Marie-Laure <marie-laure.riedinger@le-campus-numerique.fr>
2020-03-20 17:13:03 +0100

    Modifs pour le CSS

--- 1-decouvrir-DOM-jquery/niveau4/script.js
+++ 1-decouvrir-DOM-jquery/niveau4/script.js
@@ -2,14 +2,14 @@ var listOfMovie = [];
 
 $(document).ready(function(){
   $('#refresh').click(function(){                 // Au clic sur le bouton "refresh"
-  $('#list').html('');                         // Vide la liste
+  $('#list > .container > .row').html('');        // Vide la liste
   listOfMovie = [];                               // Vide le tableau avec la liste de films
   
   $.get('./playlist.txt', function(data){         // Récupère la liste dans le fichier txt
       
     splitFile(data).forEach(element => {addMovie(createMovie(element[0], element[1], element[2]));    // Chaque élément retourné par splitFile vient alimenter l'objet movie créé et le tout est poussé dans le tableau listOfMovie 
     });
-    listOfMovie.forEach(element => {$('#list').append(htmlDivElement(element));              // Affiche chaque élément du tableau listOfMovie dans une <div> avec son index, nom et durée
+    listOfMovie.forEach(element => {$('#list > .container > .row').append(htmlDivElement(element));              // Affiche chaque élément du tableau listOfMovie dans une <div> avec son index, nom et durée
     createPlayCallback(element);
   })
   });
@@ -17,7 +17,7 @@ $(document).ready(function(){
 });
 
 function htmlDivElement(movie){
-  return '<div class="divFilm"><div class="divIndex">'+ movie.index + '</div><div class="divTitle">' + movie.name + '</div><div class="divLength">' + movie.length + '</div><div><button id="playButton'+ movie.index +'">Play</button></div></div>';
+  return '<div class="divFilm col-3"><div class="divIndex">'+ movie.index + '</div><div class="divTitle">' + movie.name + '</div><div class="divLength">' + movie.length + '</div><div class="playButton"><button id="playButton'+ movie.index +'">Play</button></div></div>';
 }
 
 function createPlayCallback(movie){
